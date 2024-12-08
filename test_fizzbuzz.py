69eedcb4138ed7c089dd7053ffa06b7a6064db46
田中大輔 <tanakadaisuke@home.local>
2024-12-08 22:03:01 +0900

    入力をランダムに与えるようにテストを修正

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,18 +1,33 @@
 import pytest
+import random
 from fizzbuzz import fizzbuzz
 
+MULTIPLES_OF_15 = [15 * x for x in range(1, 67)]
+MULTIPLES_OF_3 = [3 * x for x in range(1, 334) if 3 * x not in MULTIPLES_OF_15]
+MULTIPLES_OF_5 = [5 * x for x in range(1, 201) if 5 * x not in MULTIPLES_OF_15]
+EXCLUDED_NUMBERS = set(MULTIPLES_OF_3) | set(MULTIPLES_OF_5) | set(MULTIPLES_OF_15)
+OTHER_NUMBERS = list(set(range(1, 1001)) - EXCLUDED_NUMBERS)
+
 def test_fizz_for_multiples_of_3():
-  assert fizzbuzz(3) == "Fizz"
+  for _ in range(3):
+    n = random.choice(MULTIPLES_OF_3)
+    print(f"Testing Fizz for: {n}")
+    assert fizzbuzz(n) == "Fizz", f"Failed for input: {n}"
 
 def test_buzz_for_multiples_of_5():
-  assert fizzbuzz(5) == "Buzz"
+  for _ in range(3):
+    n = random.choice(MULTIPLES_OF_5)
+    print(f"Testing Buzz for: {n}")
+    assert fizzbuzz(n) == "Buzz", f"Failed for input: {n}"
 
 def test_fizzbuzz_for_multiples_of_15():
-  assert fizzbuzz(15) == "FizzBuzz"
+  for _ in range(3):
+    n = random.choice(MULTIPLES_OF_15)
+    print(f"Testing FizzBuzz for: {n}")
+    assert fizzbuzz(n) == "FizzBuzz", f"Failed for input: {n}"
 
 def test_return_number_as_string_for_other_numbers():
-  assert fizzbuzz(1) == "1"
-  assert fizzbuzz(2) == "2"
-  assert fizzbuzz(998) == "997"
-  assert fizzbuzz(998) == "998"
-
+  for _ in range(3):
+    n = random.choice(OTHER_NUMBERS)
+    print(f"Testing Other Number for: {n}")
+    assert fizzbuzz(n) == str(n), f"Failed for input: {n}"
