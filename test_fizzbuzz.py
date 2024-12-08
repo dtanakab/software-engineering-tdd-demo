da6d89de4e15cbe65bb9854f87016b0a0f16e602
田中大輔 <tanakadaisuke@home.local>
2024-12-08 21:44:03 +0900

    3と5の倍数以外の場合は、その数字を文字列として返すテストケースを用意

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -9,3 +9,10 @@ def test_buzz_for_multiples_of_5():
 
 def test_fizzbuzz_for_multiples_of_15():
   assert fizzbuzz(15) == "FizzBuzz"
+
+def test_return_number_as_string_for_other_numbers():
+  assert fizzbuzz(1) == "1"
+  assert fizzbuzz(2) == "2"
+  assert fizzbuzz(998) == "997"
+  assert fizzbuzz(998) == "998"
+
